3179fe2aa14d7421e7f2f9e604bb5a26ead780c9
Emmanuel Castaño Sepúlveda <ecastanos@eafit.edu.co>
2026-09-12 11:47:36 -0500

    agregar IP publica de EC2 a ALLOWED_HOSTS

--- TEIS-DjangoSOLID/Tienda/settings.py
+++ TEIS-DjangoSOLID/Tienda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6to$5m=6=aoi5e#omz-$c$j^rvkw6c57=+j9a(l$e+tk6x@l^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.201.226.170', 'localhost', '127.0.0.1']
 
 
 # Application definition
